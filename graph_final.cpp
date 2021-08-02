899e02ce86dd63100d50b8d543a27eab2da0716a
LeechCSE <leechwoo613@gmail.com>
2021-08-02 16:23:04 +0900

    Initial update for graph_final.cpp
    Instead of using adjacent list, this graph library is using a list of edge. Since it stores edges themselves, the complexity of implementation is way less than one of using adjacency list.

---
+++ graph_final.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+struct Edge{
+  int src, dst;
+  T weight;
+};
+
+template <typename T>
+class Graph{
+public:
+  // Constructor
+  Graph(int n);
+  // Return number of vertices
+  int vertices();
+  // Add a new edge
+  void add_edge(Edge e);
+  // Return edges from vertex u
+  vector<Edge<T> > get_edges_of(int u);
+private:
+  int num_vertex;
+  vector<Edge<T> > edgeList;
+};
+
+// Constructor
+template <typename T>
+Graph::Graph(int n) : num_vertex(n){}
+// Return number of vertices
+template <typename T>
+int Graph::vertices(){ return num_vertex; }
+// Add a new edge
+template <typename T>
+void Graph::add_edge(Edge<T> e){
+  if (e.src >= 1 && e.dst >= 1 && e.src <= num_vertex && e.dst <= num_vertex)
+    edgeList.emplace_back(e);
+}
+// Return edges from vertex u
+template <typename T>
+vector<Edge<T> > Graph::get_edges_of(int u){
+  vector<Edge<T> > rtn;
+  for (auto e : edgeList)
+    if (e.src == u)
+      rtn.emplace_back(e);
+
+  return rtn;
+}
