b9e548ee971aa6d8937b82f54112ca25af485936
LeechCSE <leechwoo613@gmail.com>
2021-08-02 16:28:25 +0900

    Fix errors by specifying template typename T

--- graph_final.cpp
+++ graph_final.cpp
@@ -17,7 +17,7 @@ public:
   // Return number of vertices
   int vertices();
   // Add a new edge
-  void add_edge(Edge e);
+  void add_edge(Edge<T> e);
   // Return edges from vertex u
   vector<Edge<T> > get_edges_of(int u);
 private:
@@ -27,19 +27,19 @@ private:
 
 // Constructor
 template <typename T>
-Graph::Graph(int n) : num_vertex(n){}
+Graph<T>::Graph(int n) : num_vertex(n){}
 // Return number of vertices
 template <typename T>
-int Graph::vertices(){ return num_vertex; }
+int Graph<T>::vertices(){ return num_vertex; }
 // Add a new edge
 template <typename T>
-void Graph::add_edge(Edge<T> e){
+void Graph<T>::add_edge(Edge<T> e){
   if (e.src >= 1 && e.dst >= 1 && e.src <= num_vertex && e.dst <= num_vertex)
     edgeList.emplace_back(e);
 }
 // Return edges from vertex u
 template <typename T>
-vector<Edge<T> > Graph::get_edges_of(int u){
+vector<Edge<T> > Graph<T>::get_edges_of(int u){
   vector<Edge<T> > rtn;
   for (auto e : edgeList)
     if (e.src == u)
