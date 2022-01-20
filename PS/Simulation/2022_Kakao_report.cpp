0385fa33724fd1b69c97d20210835f99a4e1aef8
LeechCSE <leechwoo613@gmail.com>
2022-01-20 12:16:38 +0900

    Initial commit for 2022 Kakao Blind Recruitment: 신고 결과 받기

---
+++ PS/Simulation/2022_Kakao_report.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+struct Report{
+    string reporter, reported;
+};
+
+Report split(string r){
+    size_t pos = r.find(" ");
+    return Report{r.substr(0, pos), r.substr(pos + 1)};
+}
+
+vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    vector<int> answer(id_list.size(), 0);
+    
+    map<string, set<string>> who_reported; // id: [user who reported this.id, ...]
+    map<string, int> num_reported; // id: #reported
+    map<string, int> index_map; // id: given index
+    for (int i = 0; i < id_list.size(); i++){
+        index_map[id_list[i]] = i;
+        num_reported[id_list[i]] = 0;
+    }
+    
+    for (auto r: report){
+        Report cur = split(r);
+        
+        auto it = who_reported[cur.reported].find(cur.reporter);
+        if (it == who_reported[cur.reported].end()){
+            num_reported[cur.reported]++;
+            who_reported[cur.reported].insert(cur.reporter);
+        }
+    }
+    
+    for (auto el: num_reported){
+        string reported = el.first;
+        
+        if (el.second >= k){
+            for (auto x: who_reported[reported])
+                answer[index_map[x]]++;
+        }
+    }
+    
+    return answer;
+}
